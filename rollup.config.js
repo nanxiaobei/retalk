9e113b590ddb31d1969f8780ea49f37580c0cd7a
nanxiaobei <nanxiaobei@gmail.com>
2018-06-10 17:33:17 +0800

    :beers: Refine rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,37 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
-import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 
 const env = process.env.NODE_ENV;
-
 const config = {
-  input: 'src/retalk.js',
-  external: ['react', 'redux', 'react-redux'],
-  output: {
-    format: 'umd',
-    name: 'Retalk',
-  },
-  plugins: [
+  input: 'src/index.js',
+  external: ['react-redux'],
+  plugins: [],
+};
+
+if (env === 'es' || env === 'cjs') {
+  config.output = { format: env };
+  config.plugins.push(
+    babel({
+      plugins: ['external-helpers'],
+    }),
+  );
+}
+
+if (env === 'development' || env === 'production') {
+  config.output = { format: 'umd', name: 'Retalk' };
+  config.plugins.push(
     resolve(),
     babel({
       exclude: 'node_modules/**',
+      plugins: ['external-helpers'],
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env),
     }),
-    commonjs(),
-  ],
-};
+  );
+}
 
 if (env === 'production') {
   config.plugins.push(
