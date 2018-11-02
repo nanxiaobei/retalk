cde145a24592aaf1a5fac20f9a7e882e7700bb3a
nanxiaobei <nanxiaobei@gmail.com>
2018-11-02 16:35:18 +0800

    :white_check_mark: Update test file

--- test/index.test.js
+++ test/index.test.js
@@ -47,28 +47,29 @@ describe('retalk', () => {
 
   describe('withStore', () => {
     it('should throw error if model name is not valid', () => {
+      const connect = (mapState, mapMethods) => {
+        mapState({ test: {} });
+        mapMethods({ test: {} });
+        return {};
+      };
       expect(() => {
-        withStore();
+        connect(...withStore());
       }).toThrow(
         error.INVALID_MODEL_NAME(),
       );
       expect(() => {
-        withStore(['test'])[0]();
+        connect(...withStore(['test']));
       }).toThrow(
         error.INVALID_MODEL_NAME(),
       );
       expect(() => {
-        withStore('test', 123)[0]();
+        connect(...withStore('test', 123));
       }).toThrow(
         error.INVALID_MODEL_NAME(),
       );
       expect(Array.isArray(withStore('test'))).toBeTruthy();
       expect(withStore('test').length).toBe(2);
-      const connect = (mapState, mapMethods) => {
-        mapState({ test: { a: 1 } });
-        mapMethods({ test: { add: () => {} } });
-      };
-      expect(connect(...withStore('test'))).toBeUndefined();
+      expect(connect(...withStore('test'))).toEqual({});
     });
   });
 });
