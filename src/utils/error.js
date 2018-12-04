5bf1c6b5fcf40ff31794dd62c0c8089eec203576
nanxiaobei <nanxiaobei@gmail.com>
2018-12-04 21:34:30 +0800

    :rotating_light: Add error type

--- src/utils/error.js
+++ src/utils/error.js
@@ -3,17 +3,20 @@
  */
 const error = {
   // common
-  NOT_STRING: name => `${name} must be a string`,
-  NOT_OBJECT: name => `${name} must be an object`,
+  NOT_STRING: (name) => `${name} must be a string`,
+  NOT_BOOLEAN: (name) => `${name} must be a boolean`,
+  NOT_ARRAY: (name) => `${name} must be an array`,
+  NOT_OBJECT: (name) => `${name} must be an object`,
   // index
-  INVALID_IMPORTER: name => `Invalid () => import() function to ${name} model`,
+  INVALID_IMPORTER: (name) => `Invalid () => import() function to ${name} model`,
   INVALID_MODEL_NAME: () => 'Params to withStore() must all be string',
   // createReducer
   NO_MODEL_KEY: (name, key) => `${key} [object] is required in ${name} model`,
-  INVALID_REDUCERS: name => `reducers must be an object (in ${name} model)`,
+  INVALID_REDUCERS: (name) => `reducers must be an object (in ${name} model)`,
   // createMethods
   ASYNC_REDUCER: (name, reducer) => `Reducer can not be an async function (${name}.${reducer})`,
-  METHODS_CONFLICT: (name, reducer) => `Name duplicated in reducers and actions (${name}.${reducer})`,
+  METHODS_CONFLICT: (name, reducer) =>
+    `Name duplicated in reducers and actions (${name}.${reducer})`,
   // utils/methodsStation
   INVALID_ACTION: () => 'Please do not use dispatch in Retalk',
 };
