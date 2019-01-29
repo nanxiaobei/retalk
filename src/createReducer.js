2a886fbfaac5ff73b0efcb5d4fa6aa3b8bec2275
nanxiaobei <nanxiaobei@gmail.com>
2019-01-29 21:18:21 +0800

    ♻️ Simplify logic, remove `reducers` support

--- src/createReducer.js
+++ src/createReducer.js
@@ -1,50 +1,16 @@
-import isObject from './utils/isObject';
-import error from './utils/error';
-
 /**
- * createReducer
+ * Create model's reducer
+ *
  * @param {string} name
- * @param {Object} model
- * @returns {Function} Reducer
+ * @param {object} state
+ * @return {function} Reducer
  */
-const createReducer = (name, model) => {
-  if (typeof name !== 'string') {
-    throw new Error(error.NOT_STRING('name'));
-  }
-  if (!isObject(model)) {
-    throw new Error(error.NOT_OBJECT('model'));
-  }
-  const { state, reducers, actions } = model;
-  if (!isObject(state)) {
-    throw new Error(
-      state === undefined ? error.NO_MODEL_KEY(name, 'state') : error.NOT_OBJECT(name, 'state'),
-    );
-  }
-  if (reducers !== undefined) {
-    // eslint-disable-next-line
-    console.warn(
-      'Since Retalk now support different action types between "actions" in v1.2.1, "reducers" will be deprecated soon, please don\'t use it.',
-    );
-    if (!isObject(reducers)) {
-      throw new Error(error.INVALID_REDUCERS(name));
-    }
-  }
-  if (!isObject(actions)) {
-    throw new Error(
-      actions === undefined
-        ? error.NO_MODEL_KEY(name, 'actions')
-        : error.NOT_OBJECT(name, 'actions'),
-    );
-  }
-
+const createReducer = (name, state) => {
+  state.loading = {};
   return (currentState = state, action) => {
-    const [modelName, methodName, setStateLabel] = action.type.split('/');
+    const [modelName] = action.type.split('/');
     if (modelName !== name) return currentState;
-
-    if (setStateLabel === 'SET_STATE') {
-      return { ...currentState, ...action.partialState };
-    }
-    return reducers[methodName](currentState, ...action.payload);
+    return { ...currentState, ...action.partialState };
   };
 };
 
