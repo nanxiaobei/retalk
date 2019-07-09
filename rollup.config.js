10625c81651b9ac7ac423245b5ccd64aeeb2d641
nanxiaobei <nanxiaobei@gmail.com>
2019-07-10 02:39:34 +0800

    🌀 Update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
 import { eslint } from 'rollup-plugin-eslint';
 import babel from 'rollup-plugin-babel';
 
-import pkg from './package.json';
+import { dependencies } from './package.json';
 
 const { NODE_ENV } = process.env;
 
-const dependencies = [...Object.keys(pkg.peerDependencies), ...Object.keys(pkg.dependencies)];
+const depKeys = Object.keys(dependencies);
 
 const config = {
   input: 'src/index.js',
   output: { format: NODE_ENV, indent: false },
   external: (id) => {
-    if (dependencies.includes(id)) return true;
+    if (depKeys.includes(id)) return true;
     if (id.includes('@babel/runtime/')) return true;
   },
   plugins: [
