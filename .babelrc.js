a88f2c91ebb4fa4653a88e79fd7745ff99f450d4
nanxiaobei <nanxiaobei@gmail.com>
2019-08-04 07:26:35 +0800

    🍗 Change `.babelrc.js` to `.babelrc`

--- .babelrc.js
+++
@@ -1,9 +0,0 @@
-const { NODE_ENV } = process.env;
-
-module.exports = {
-  presets: [['@babel/preset-env', { modules: false }]],
-  plugins: [
-    NODE_ENV === 'test' && '@babel/plugin-transform-modules-commonjs',
-    ['@babel/plugin-transform-runtime', { useESModules: NODE_ENV === 'esm' }],
-  ].filter(Boolean),
-};
