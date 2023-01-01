f19f1846effb3ed5af967d52a8443fb1343ebb4c
nanxiaobei <nanxiaobei@gmail.com>
2023-01-01 22:55:25 +0800

    update babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,5 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: '> 0.25%, not dead', modules: false }]],
-  env: {
-    test: {
-      plugins: [
-        '@babel/plugin-transform-modules-commonjs',
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-transform-react-jsx',
-      ],
-    },
-  },
+  presets: [
+    ['@babel/preset-env', { targets: '> 0.25%, not dead', modules: false }],
+  ],
 };
