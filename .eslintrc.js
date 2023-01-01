6285418ea8ff94a17bf4cbc049bf5d6d8fd61891
nanxiaobei <nanxiaobei@gmail.com>
2023-01-01 19:50:40 +0800

    update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,3 @@
 module.exports = {
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  overrides: [
-    {
-      files: '*.test.js',
-      env: {
-        jest: true,
-      },
-    },
-  ],
+  extends: ['react-app', 'prettier'],
 };
