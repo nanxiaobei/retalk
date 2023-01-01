3152de1ace80fd2adca16390e75e9037d521c888
nanxiaobei <nanxiaobei@gmail.com>
2023-01-01 22:55:34 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,18 @@ import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 const input = 'src/index.js';
+const cjsOutput = { file: pkg.main, format: 'cjs' };
+const esmOutput = { file: pkg.module, format: 'es' };
 const deps = Object.keys(pkg.dependencies);
 const external = (id) => deps.includes(id) || id.includes('@babel/runtime/');
 const plugins = (useESModules) => [
-  babel({ plugins: [['@babel/plugin-transform-runtime', { useESModules }]], runtimeHelpers: true }),
+  babel({
+    plugins: [['@babel/plugin-transform-runtime', { useESModules }]],
+    runtimeHelpers: true,
+  }),
 ];
 
 export default [
-  { input, output: { file: pkg.main, format: 'cjs' }, external, plugins: plugins(false) },
-  { input, output: { file: pkg.module, format: 'es' }, external, plugins: plugins(true) },
+  { input, output: cjsOutput, external, plugins: plugins(false) },
+  { input, output: esmOutput, external, plugins: plugins(true) },
 ];
