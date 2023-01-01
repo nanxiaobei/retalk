04b072505b572e3e8b35f632f8d13708609b64bc
nanxiaobei <nanxiaobei@gmail.com>
2023-01-01 20:02:18 +0800

    update configs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,17 @@
 module.exports = {
-  printWidth: 100,
   singleQuote: true,
+
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  importOrder: [
+    '^react',
+    '^rollup',
+    '^@rollup',
+    '^vite',
+    '^@vite',
+    '<THIRD_PARTY_MODULES>',
+    '^[./]',
+  ],
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderCaseInsensitive: true,
 };
